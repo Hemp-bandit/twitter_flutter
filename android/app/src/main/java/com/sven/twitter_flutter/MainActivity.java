4a3b40d15f4230cd16d2748d1e930162c7069c13
1208879283@qq.com <1208879283@qq.com>
2021-02-26 12:44:24 +0800

    modify: 将Android工程编译语言修改为java

---
+++ android/app/src/main/java/com/sven/twitter_flutter/MainActivity.java
@@ -0,0 +1,6 @@
+package com.sven.twitter_flutter;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/sven/twitter_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sven.twitter_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
